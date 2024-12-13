aa7fdd6a3109e544a94f530abcaa8565e339cc0d
Taesok Kwon <kwontaesok1997@gmail.com>
2024-12-13 00:07:00 -0800

    refactor: add header sanitization

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use PlantUML\Exceptions\HttpException;
 
-$routes = include '../src/Routing/routes.php';
+$routes = include '../src/routes.php';
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = ltrim($path, '/');
 
@@ -14,17 +14,27 @@ if (php_sapi_name() === 'cli-server') {
     }
 }
 
-try {
-    if (!isset($routes[$path])) {
-        throw new HttpException(404, 'The requested page was not found');
+if (isset($routes[$path])) {
+    try {
+        $renderer = $routes[$path]();
+        // Set raw HTTP headers
+        foreach ($renderer->getFields() as $name => $value) {
+            $sanitized_value = htmlspecialchars($value, ENT_NOQUOTES, 'UTF-8');
+            if ($sanitized_value && $sanitized_value === $value) {
+                header("{$name}: {$sanitized_value}");
+            } else {
+                throw new Exception('Failed setting header - original: ' . $value . ', sanitized: ' . $sanitized_value);
+            }
+        }
+        echo $renderer->getContent();
+    } catch (HttpException $e) {
+        http_response_code($e->getStatusCode());
+        echo $e->getStatusCode() . ' ' . $e->getErrorMessage();
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo $e->getMessage();
     }
-
-    $renderer = $routes[$path]();
-    echo $renderer->getContent();
-} catch (HttpException $e) {
-    http_response_code($e->getStatusCode());
-    echo $e->getStatusCode() . ' ' . $e->getErrorMessage();
-} catch (Exception $e) {
-    http_response_code(500);
-    echo $e->getMessage();
+} else {
+    http_response_code(404);
+    echo '404 Not Found: The requested route was not found on this server.';
 }
