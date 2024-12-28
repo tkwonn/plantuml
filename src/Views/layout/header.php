675dd677478fd715c5ad5bcc610e5c9feef91879
Taesok Kwon <kwontaesok1997@gmail.com>
2024-12-28 15:36:53 -0800

    Add favicon

--- src/Views/layout/header.php
+++ src/Views/layout/header.php
@@ -10,6 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/assets/img/favicon.ico">
     <title><?php echo htmlspecialchars($pageTitle); ?></title>
 
     <!-- Bootstrap CSS -->
